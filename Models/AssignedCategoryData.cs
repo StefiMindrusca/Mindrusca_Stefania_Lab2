479db75bf17a539de07600c52068114f5b15916b
StefiMindrusca <stefi.mindrusca@gmail.com>
2025-01-01 20:04:05 +0200

    Delete Models directory

--- Models/AssignedCategoryData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mindrusca_Stefania_Lab2.Models
-{
-    public class AssignedCategoryData
-    {
-        public int CategoryID { get; set; }
-        public string Name { get; set; }
-        public bool Assigned { get; set; }
-    }
-}
